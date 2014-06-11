6e9e3bea8fdd7180e923f0b2359cdb1214306379
Karan Sivarat <bomb0069@gmail.com>
2014-06-11 13:55:58 -0400

    refactor test by grouping case that match with rule

--- src/test/java/th/own/bomb0069/kata/RomanNumberTest.java
+++ src/test/java/th/own/bomb0069/kata/RomanNumberTest.java
@@ -8,22 +8,14 @@ public class RomanNumberTest {
     NumberConvertor numberConvertor = NumberConvertorFactory.create();
 
     @Test
-    public void test1ShouldBeReturnI() {
+    public void testIRule() {
         assertEquals("I", numberConvertor.getRomanNumber(1));
-    }
-
-    @Test
-    public void test2ShouldBeReturnII() {
         assertEquals("II", numberConvertor.getRomanNumber(2));
-    }
-
-    @Test
-    public void test3ShouldBeReturnIII() {
         assertEquals("III", numberConvertor.getRomanNumber(3));
     }
 
     @Test
-    public void test4ShouldBeReturnIV() {
+    public void testIVRule() {
         assertEquals("IV", numberConvertor.getRomanNumber(4));
     }
 }
