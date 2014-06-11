a064e26aa13588bfcfce42f1b452b8b221fc7f44
Karan Sivarat <bomb0069@gmail.com>
2014-06-11 14:04:32 -0400

    refactor test to use assertRomanArabic

--- src/test/java/th/own/bomb0069/kata/RomanNumberTest.java
+++ src/test/java/th/own/bomb0069/kata/RomanNumberTest.java
@@ -7,25 +7,33 @@ public class RomanNumberTest {
 
     NumberConvertor numberConvertor = NumberConvertorFactory.create();
 
+    private void assertRomanArabic(String roman, int arabic) {
+        assertEquals(roman, numberConvertor.getRomanNumber(arabic));
+    }
+
     @Test
     public void testIRule() {
-        assertEquals("I", numberConvertor.getRomanNumber(1));
-        assertEquals("II", numberConvertor.getRomanNumber(2));
-        assertEquals("III", numberConvertor.getRomanNumber(3));
+        assertRomanArabic("I", 1);
+        assertRomanArabic("II", 2);
+        assertRomanArabic("III", 3);
     }
 
     @Test
     public void testIVRule() {
-        assertEquals("IV", numberConvertor.getRomanNumber(4));
+        assertRomanArabic("IV", 4);
     }
 
     @Test
     public void testVRule() {
-        assertEquals("V", numberConvertor.getRomanNumber(5));
+        assertRomanArabic("V", 5);
+        assertRomanArabic("VI", 6);
+        assertRomanArabic("VII", 7);
+        assertRomanArabic("VIII", 8);
     }
 
     @Test
     public void testIXRule() {
-        assertEquals("IX", numberConvertor.getRomanNumber(9));
+        assertRomanArabic("IX", 9);
     }
+
 }
